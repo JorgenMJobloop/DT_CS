87b218afd684221897ba4b58f976a65eca3fdcb4
JorgenMJobloop <jorgen@jobloop.no>
2024-01-05 13:43:59 +0100

    Add files via upload

---
+++ 2024/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // if we use a type of dynamic CLI solution, that takes user input in a variable and opens up the I/O system dialog.
+            // we can then prompt the user to give a full path of the file that is to be read.
+            
+
+            Console.WriteLine("Give a filepath: ");
+            string filePath = Console.ReadLine();
+            Console.WriteLine("Filename: ");
+            string fileName = Console.ReadLine();
+            
+            try {
+
+                string fullPath = Path.Combine(filePath, fileName);
+                using (StreamWriter newWriter = new StreamWriter(fullPath)) {
+                    newWriter.WriteLine("l1: Hello from C#!");
+                    newWriter.WriteLine("l2: Access to path denied?");
+                }
+                Console.WriteLine("Written to file");
+
+                // try to read the content of the file
+                string fileContent = File.ReadAllText(filePath + "\\" + fileName);
+                Console.WriteLine(fileContent);
+
+            }
+
+            catch(IOException e) {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
