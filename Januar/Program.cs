2f30d5a876be2f09008016541a5ee502d5334c59
JorgenMJobloop <jorgen@jobloop.no>
2024-01-26 11:07:14 +0100

    Add files via upload

---
+++ Januar/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace DT24
+{
+    internal class Program
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        // static void main() er selve programmet , her kjøres all koden og her må all koden kalles fra/til.
+        static void Main(string[] args)
+        {
+            // code goes here
+            // kall til Sum metoden
+            Console.WriteLine(Sum(1, 3));
+            int x = 5;
+            double y = 3.2;
+            float z = 1;
+            Console.WriteLine(x + y + z);
+
+            string helloWorld = "Hello World!";
+
+            // vi legger til [] square brackets, for å signalisere at det er et string array
+            string[] myArray = { "Hello", "World", "From", "A", "C#", "Array!" };
+
+            int[] myIntArray = { 1, 2, 3 };
+
+            Console.WriteLine(helloWorld);
+
+            // foreach loops
+            foreach (string arrays in myArray)
+            {
+                Console.WriteLine(arrays);
+            }
+
+
+            foreach (int myNumber in myIntArray)
+            {
+                Console.WriteLine(myNumber);
+            }
+
+        }
+    }
+
+}
---
+++ Januar/eksempler.md
@@ -0,0 +1,12 @@
+```cs
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            Console.WriteLine(myArray[i]);
+        }
+
+        for (int j = 0; j < myIntArray.Count(); j++)
+        {
+            Console.WriteLine(myIntArray[j]);
+        }
+
+```
